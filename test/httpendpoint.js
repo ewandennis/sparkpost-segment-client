007d97e2878076004a227b6aa698a22b38405e55
Ewan Dennis <ewandennis@users.noreply.github.com>
2015-08-07 15:00:02 +0100

    The SparkPost webhook endpoint is now /api/v1/events Patched silly async control flow issue with Segment REST calls Upped test logging verbosity Cleaned up and improved tests

--- test/httpendpoint.js
+++ test/httpendpoint.js
@@ -29,8 +29,6 @@ var TEST_EVENTS_1 = require('./testevents1.json');
 // Transmission API: reception, tempfail, tempfail, inband
 var TEST_EVENTS_2 = require('./testevents2.json');
 
-var cxt;
-
 function TestContext() {
   return {
     segmentClient: null,
@@ -40,16 +38,25 @@ function TestContext() {
 
     start: function(app, next) {
       if (process.env.NODE_ENV == 'livetest') {
-        console.log("*** LIVE TEST MODE ***");
         this.segmentClient = new segmentLib(config.get('segmentAPI.key'), config.get('segmentAPI.opts'));
         sinon.spy(this.segmentClient, 'identify');
         sinon.spy(this.segmentClient, 'track');
       } else {
         this.segmentClient = {
-          identify: sinon.spy(),
-          track: sinon.spy(),
+          identify: sinon.spy(function (args, next) {
+            process.nextTick(
+              function() { next(null); }
+            );
+          }),
+          track: sinon.spy(function (args, next) {
+            process.nextTick(
+              function() { next(null); }
+            );
+          }),
           flush: sinon.spy(function (next) {
-            process.nextTick(next);
+            process.nextTick(
+              function() { next(null); }
+            );
           })
         };
       }
@@ -61,61 +68,74 @@ function TestContext() {
 
     stop: function (next) {
       this.server.close(next);
+    },
+
+    callInboundEndpoint: function (events, next) {
+      request({
+        uri: 'http://localhost:3000/api/v1/events',
+        method: 'POST',
+        json: events
+      }).on('response', next);
+    },
+
+    testResponseToEventTypes: function (eventset, evtTypes, next) {
+      var self = this;
+      var events;
+      if (typeof evtTypes == 'string') {
+        events = eventset.filter(function (elt) {
+          return elt.msys.message_event && elt.msys.message_event.type == evtTypes;
+        });
+      } else {
+        // assumption: evtTypes is an array of strings
+        events = eventset.filter(function (elt) {
+          var type;
+          if (elt.msys.message_event) {
+            type = elt.msys.message_event.type;
+          } else if (elt.msys.track_event) {
+            type = elt.msys.track_event.type;
+          } else if (elt.msys.gen_event) {
+            type = elt.msys.gen_event.type;
+          } else {
+            console.warn('Unexpected event class: ' + JSON.stringify(Object.keys(elt.msys)));
+            return false;
+          }
+          return evtTypes.indexOf(type) >= 0;
+        });
+      }
+
+      self.callInboundEndpoint(events, function (resp) {
+        self.app.flushSegmentCache(function (err, batch) {
+          assert(err == null, 'Segment.flush failed: ' + err);
+          next(resp, err, batch);
+        });
+      });
     }
   };
 }
 
-function callInboundEndpoint(events, next) {
-  request({
-    uri: 'http://localhost:3000/inbound',
-    method: 'POST',
-    json: events
-  }).on('response', next);
-}
-
-function testResponseToEventTypes(eventset, evtTypes, next) {
-  var events;
-  if (typeof evtTypes == 'string') {
-    events = eventset.filter(function (elt) {
-      return elt.msys.message_event && elt.msys.message_event.type == evtTypes;
-    });
-  } else {
-    // assumption: evtTypes is an array of strings
-    events = eventset.filter(function (elt) {
-      var type;
-      if (elt.msys.message_event) {
-        type = elt.msys.message_event.type;
-      } else if (elt.msys.track_event) {
-        type = elt.msys.track_event.type;
-      } else if (elt.msys.gen_event) {
-        type = elt.msys.gen_event.type;
-      } else {
-        console.warn('Unexpected event class: ' + JSON.stringify(Object.keys(elt.msys)));
-        return false;
-      }
-      return evtTypes.indexOf(type) >= 0;
-    });
-  }
-
-  callInboundEndpoint(events, next);
-}
-
 function testPrep() {
-  cxt = new TestContext();
-  cxt.start();
+  this.cxt = new TestContext();
+  this.cxt.start();
 }
 
 function testCleanup(next) {
-  cxt.stop(next ? next : function () {});
-  cxt = null;
+  this.cxt.stop(next);
+  this.cxt = null;
 }
+
 // ----------------------------------------------------------------------------
+
+if (process.env.NODE_ENV == 'livetest') {
+  console.log("*** LIVE TEST MODE ***");
+}
+
 beforeEach('Start new webhook endpoint service', testPrep);
 afterEach('Shutdown webhook endpoint service', testCleanup);
 
 describe('SparkPost webhook endpoint', function () {
   it('/inbound accepts JSON POST requests', function (done) {
-    callInboundEndpoint(TEST_EVENTS_1, function (resp) {
+    var self = this;
+    self.cxt.callInboundEndpoint(TEST_EVENTS_1, function (resp) {
       expect(resp.statusCode).to.equal(200);
       done();
     });
@@ -131,82 +151,68 @@ describe('SparkPost webhook endpoint', function () {
 
 describe('Segment.com client', function () {
   it('makes 1 segment.identify call per inbound email address', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_1, 'reception', function (resp) {
-      cxt.app.flushSegmentCache(function (err, batch) {
-        assert(err == null, 'Segment.flush failed: ' + err);
-        cxt.segmentClient.identify.should.have.callCount(1);
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_1, 'reception', function (resp, err, batch) {
+      self.cxt.segmentClient.identify.should.have.callCount(1);
+      done();
     });
   });
 
   it('makes 1 segment.track("Email Delivered") call for each received delivery event', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery'], function (resp) {
-      cxt.app.flushSegmentCache(function (err, batch) {
-        assert(err == null, 'Segment.flush failed: ' + err);
-        cxt.segmentClient.track.should.have.callCount(1);
-        expect(cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Delivered'}));
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery'], function (resp, err, batch) {
+      self.cxt.segmentClient.track.should.have.callCount(1);
+      expect(self.cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Delivered'}));
+      done();
     });
   });
 
   it('makes 1 segment.track("Email Bounced") call for each received inband bounce event', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_2, ['reception', 'inband'], function (resp) {
-      cxt.app.flushSegmentCache(function (err, batch) {
-        assert(err == null, 'Segment.flush failed: ' + err);
-        cxt.segmentClient.track.should.have.callCount(1);
-        expect(cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Bounced'}));
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_2, ['reception', 'inband'], function (resp, err, batch) {
+      self.cxt.segmentClient.track.should.have.callCount(1);
+      expect(self.cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Bounced'}));
+      done();
     });
   });
 
   it.skip('makes 1 segment.track("Email Bounced") call for each received out_of_band event', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_2, ['reception', 'delivery', 'out_of_band'], function (resp) {
-      cxt.app.flushSegmentCache(function (err, batch) {
-        assert(err == null, 'Segment.flush failed: ' + err);
-        cxt.segmentClient.track.should.have.callCount(2);
-        expect(cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Bounced'}));
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_2, ['reception', 'delivery', 'out_of_band'], function (resp, err, batch) {
+      self.cxt.segmentClient.track.should.have.callCount(2);
+      expect(self.cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Bounced'}));
+      done();
     });
   });
 
   it.skip('makes 1 segment.track("Email Marked as Spam") call for each received feedback/abuse event', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery', 'feedback'], function (resp) {
-      cxt.segmentClient.track.should.have.callCount(2);
-      cxt.app.flushSegmentCache(function (err, batch) {
-        assert(err == null, 'Segment.flush failed: ' + err);
-        expect(cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Marked as Spam'}));
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery', 'feedback'], function (resp, err, batch) {
+      expect(self.cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Marked as Spam'}));
+      done();
     });
   });
 
   it('makes 1 segment.track("Email Opened") call for each received open event', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery', 'open'], function (resp) {
-      cxt.app.flushSegmentCache(function (err, batch) {
-        assert(err == null, 'Segment.flush failed: ' + err);
-        cxt.segmentClient.track.should.have.callCount(2);
-        expect(cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Opened'}));
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery', 'open'], function (resp, err, batch) {
+      self.cxt.segmentClient.track.should.have.callCount(2);
+      expect(self.cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Opened'}));
+      done();
     });
   });
 
   it('makes 1 segment.track("Email Link Clicked") call for each received click event', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery', 'open', 'click'], function (resp) {
-      cxt.app.flushSegmentCache(function (err, batch) {
-        assert(err == null, 'Segment.flush failed: ' + err);
-        cxt.segmentClient.track.should.have.callCount(3);
-        expect(cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Link Clicked'}));
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_1, ['reception', 'delivery', 'open', 'click'], function (resp, err, batch) {
+      self.cxt.segmentClient.track.should.have.callCount(3);
+      expect(self.cxt.segmentClient.track).to.be.calledWith(sinon.match({event: 'Email Link Clicked'}));
+      done();
     });
   });
 
   it('drops delivery events whose message_ids do not have cached rcpt_tos', function (done) {
+    var self = this;
     var evt = {
       msys: {
         message_event: {
@@ -216,39 +222,42 @@ describe('Segment.com client', function () {
       }
     }
 
-    callInboundEndpoint([evt], function (resp) {
-      expect(cxt.segmentClient.track.callCount).to.equal(0);
-      done();
+    self.cxt.callInboundEndpoint([evt], function (resp) {
+      self.cxt.app.flushSegmentCache(function (err, batch) {
+        self.cxt.segmentClient.identify.should.have.callCount(0);
+        self.cxt.segmentClient.track.should.have.callCount(0);
+        done();
+      });
     });
   });
 
   it('drops open events whose message_ids do not have cached rcpt_tos', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_1, ['open'], function (resp) {
-      cxt.app.flushSegmentCache(function () {
-        cxt.segmentClient.track.should.have.callCount(0);
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_1, ['open'], function (resp, err, batch) {
+      self.cxt.segmentClient.identify.should.have.callCount(0);
+      self.cxt.segmentClient.track.should.have.callCount(0);
+      done();
     });
   });
 
   it('drops click events whose message_ids do not have cached rcpt_tos', function (done) {
-    testResponseToEventTypes(TEST_EVENTS_1, ['click'], function (resp) {
-      cxt.app.flushSegmentCache(function () {
-        cxt.segmentClient.track.should.have.callCount(0);
-        done();
-      });
+    var self = this;
+    self.cxt.testResponseToEventTypes(TEST_EVENTS_1, ['click'], function (resp, err, batch) {
+      self.cxt.segmentClient.identify.should.have.callCount(0);
+      self.cxt.segmentClient.track.should.have.callCount(0);
+      done();
     });
   });
 
   it('is resilient to non-array json requests', function (done) {
-    callInboundEndpoint({invalid:'object'}, function (resp) {
+    this.cxt.callInboundEndpoint({invalid:'object'}, function (resp) {
       expect(resp.statusCode).to.equal(500);
       done();
     });
   });
 
   it('quietly accepts empty json arrays in requests', function (done) {
-    callInboundEndpoint([], function (resp) {
+    this.cxt.callInboundEndpoint([], function (resp) {
       expect(resp.statusCode).to.equal(200);
       done();
     });
@@ -257,7 +266,7 @@ describe('Segment.com client', function () {
   it('ignores malformed events in requests', function (done) {
     var badbatch = _.cloneDeep(TEST_EVENTS_1);
     badbatch[0] = {invalid: 'object'};
-    callInboundEndpoint(badbatch, function (resp) {
+    this.cxt.callInboundEndpoint(badbatch, function (resp) {
       expect(resp.statusCode).to.equal(200);
       done();
     });
@@ -266,9 +275,10 @@ describe('Segment.com client', function () {
   it('ignores unexpected event types in requests', function (done) {
     var badbatch = _.cloneDeep(TEST_EVENTS_1);
     appUtils.unpackEvent(badbatch[0]).type = 'binglefloop';
-    callInboundEndpoint(badbatch, function (resp) {
+    this.cxt.callInboundEndpoint(badbatch, function (resp) {
       expect(resp.statusCode).to.equal(200);
       done();
     });
   });
 });
+
